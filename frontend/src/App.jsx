d105c47bb8d771b13e7ac2c5153522e389e5a709
knsaran45 <knsaran45@gmail.com>
2026-07-13 17:35:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
